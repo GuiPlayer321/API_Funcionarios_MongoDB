fda611890cba89367dc2e6d77b18c789e55a2c03
Guilherme de Carvalho Ferreira <64448040+GuiPlayer321@users.noreply.github.com>
2023-11-03 13:24:04 -0300

    feat: add all routes, controllers and config

---
+++ controllers/funcionario.js
@@ -0,0 +1,76 @@
+const controller = {}
+const Funcionario = require ('../models/Funcionario')
+
+controller.create = async function (req, res){
+    const {nome, cargo, salario, contratado} = req.body;
+    
+    if(!nome && !cargo && !salario && !contratado) {
+       return res.status(422).json({erro: "É obrigatório informe nome, cargo, salario e se esta ativo!"});
+    }
+    
+
+    const funcionario ={
+        nome, 
+        cargo, 
+        salario, 
+        contratado
+    };
+
+    try{
+        await Funcionario.create(funcionario);
+        res.status(201).json({message: "Funcionario inserido com sucesso!"})
+    } catch (error) {
+        res.status(500).json({error:error});
+    }
+}
+
+controller.retrieveAll = async function (req, res){
+    try{
+        const results = await Funcionario.find()
+        res.send(results)
+    }catch (error){
+        console.log(error)
+        res.status(500).json({error: error})
+    }
+}
+
+controller.retrieveOne = async function (req, res){
+    const filter = req.params.id;
+    try{
+        const result = await Funcionario.findById(filter)
+        res.send(result)
+    }catch (error){
+        console.log(error)
+        res.status(500).json({error: error})
+    }
+}
+
+controller.update = async function (req, res){
+    const filter = req.params.id;
+    const update = req.body;
+    try{
+        const result = await Funcionario.findByIdAndUpdate(filter, update, {
+            new: true,
+            returnOriginal: false
+        })
+        if (result) res.status(200).send(result);
+        else res.status(404).end();
+    }catch (error){
+        console.log(error)
+        res.status(500).json({error: error})
+    }
+}
+
+controller.delete = async function (req, res){
+    const filter = req.params.id;
+    try{
+        const result = await Funcionario.findByIdAndDelete(filter)
+        if (result) res.status(200).json({message:"Funcionario excluido conforme o solicitado"}).end();
+        else res.status(404).end();
+    }catch (error){
+        console.log(error)
+        res.status(500).json({error: error})
+    }
+}
+
+module.exports = controller;
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const fucionarioRoutes = require('./routes/funcionario.js')
+const server = express()
+server.use(express.json())
+
+server.use(
+    express.urlencoded({
+        extended:true
+    })
+)
+
+server.use('/funcionario', fucionarioRoutes)
+
+mongoose.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nnrk6ri.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`
+    )
+    .then(()=>{
+        console.log("Conexao efetuada com sucesso");
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
+
+
+
+server.listen(3001, ()=> console.log("Rodando na porta 3001"))
